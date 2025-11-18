feef8ed404b2fc3b43ee4e445407570c99c60de1
MissMaaai <nmtd10@hotmail.com>
2025-11-18 11:09:16 +0100

    Add folders: FloraBackend, FlouraApi, FlouraCore og Tests

---
+++ FlouraBackend/Floura.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Floura.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tests/Floura.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Floura.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
