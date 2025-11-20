aa2682c476b449c50365b7da740e222a13518dc2
NannaRister <nannarister@hotmail.com>
2025-11-20 08:59:22 +0100

    Add (StoryTest) added test for title, summary, coverImage and a valid-test

--- Tests/Floura.Tests/StoryTests.cs
+++ Tests/Floura.Tests/StoryTests.cs
@@ -3,24 +3,74 @@ using Floura.Core;
 
 namespace Floura.Tests
 
-{
+{   //skal have lavet en IsValid metode inde i Story.cs for at testen ikke fejler
     public class StoryTests
     {
 
         [Fact]
         public void Title_ShouldNotBeNullOrEmpty()
         {
-            var story = new Story { Title = "" };
+            //Act
+            var story = new Story 
+            { 
+                Title = "" 
+                Summary = "Summary test"
+                CoverImage = "ImageURL"
+                AgeRange = 3
+                StoryBits = {new StoryBit() }
+            
+            };
+
             Assert.False(story.IsValid());
         }
 
         [Fact]
         public void Summary_ShouldNotBeNullOrEmpty()
         {
-            var story = new Story { Summary = "" };
+            var story = new Story
+            {
+                Title = "My Title",
+                Summary = "",
+                CoverImage = "ImageURL"
+                AgeRange = 3,
+                StoryBits = { new StoryBit() }
+            };
+
             Assert.False(story.IsValid());
+        }
+
+        [Fact]
+        public void CoverImage_ShouldNotBeNullOrEmpty()
+        {
+            var story = new StoryTests
+            {
+                Title = "My Title",
+                Summary = "",
+                CoverImage = ""
+                AgeRange = 3,
+                StoryBits = { new StoryBit() }
+            };
+
+            Assert.False(story.IsValid());
+        }
+
+        [Fact]
+        public void Story_ShouldBeValid_WhenAllFieldsAreCorrect()
+        {
+            var story = new Story
+            {
+                Title = "My title",
+                Summary = "Summary Test",
+                CoverImage = "ImageURL"
+                AgeRange = 3,
+                StoryBits = { new StoryBit() }
+            };
+
+            Assert.True(story.IsValid());
         }
 
     }
 }
+
+
 
