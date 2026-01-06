65f35457807a7841f94cfa4290ec688cd343abb2
NannaRister <nannarister@hotmail.com>
2026-01-06 10:58:27 +0100

    Resolved merge conflicts

--- FlouraFrontend/FlouraMobileApp/src/screens/StoriesOverviewScreen.tsx
+++ FlouraFrontend/FlouraMobileApp/src/screens/StoriesOverviewScreen.tsx
@@ -1,71 +1,5 @@
-/* import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { storyService } from '../services/storyService';
-import { Story } from '../models/Story';
-import StoriesList from '../components/StoriesList';
-import LoadingScreen from '../components/Loading';
-import { router } from 'expo-router';
-import Title from '../components/Title';
-
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
-
-export default function StoriesOverviewScreen() {
-  const [stories, setStories] = useState<Story[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  //const [error, setError] = useState<string | null>(null);
-  const coverKids = require('../../assets/images/coverImages/coverKids.jpg');
-  const coverKidsUri = Image.resolveAssetSource(coverKids).uri;
-
-  useEffect(() => {
-    const fetchStories = async () => {
-      try {
-        const data = await storyService.getStories();
-        if (!data) {
-          throw new Error('Der er ikke nogen historie');
-        }
-
-        setStories(data.map((s) => ({ ...s, coverImageUrl: coverKidsUri })));
-      } catch {
-        router.replace('/errorScreen?message=Historierne er blevet væk');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchStories();
-  }, []);
-
-  function handlePressStory(story: Story) {
-    router.push(`../stories/${story.id}`);
-  }
-
-  function handlePressReminder(){
-    router.push('/reminderSettingsScreen')
-  }
-  if (isLoading) return <LoadingScreen />;
-
-  return (
-    <View style={styles.container}>
-      <Button title="Opsæt påmindelser" onPress={handlePressReminder} />
-      <StoriesList items={stories} onPressStory={handlePressStory} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 12,
-    paddingBottom: 12,
-    backgroundColor: '#E3F2EA',
-  },
-});
- */
-
-
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Image, Button } from 'react-native';
+import { StyleSheet, Image, Button } from 'react-native';
 import { storyService } from '../services/storyService';
 import { Story } from '../models/Story';
 import StoriesList from '../components/StoriesList';
@@ -112,6 +46,10 @@ export default function StoriesOverviewScreen() {
     router.push(`../stories/${story.id}`);
   }
 
+  function handlePressReminder(){
+    router.push('/reminderSettingsScreen');
+  }
+
   if (isLoading) return <LoadingScreen />;
 
   return (
@@ -119,6 +57,12 @@ export default function StoriesOverviewScreen() {
       <StatusBar style="dark" />
       <SafeAreaView style={styles.container} edges={['top']}>
         <Title text="Historie oversigt" />
+
+        <Button 
+          title="Opsæt påmindelser"
+          onPress={handlePressReminder}
+        />
+
         <StoriesList items={stories} onPressStory={handlePressStory} />
       </SafeAreaView>
     </>
