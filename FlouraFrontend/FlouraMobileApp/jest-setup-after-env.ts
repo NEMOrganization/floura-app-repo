7897be335241deccdd83fa6d6e1ef74e2964987e
MissMaaai <nmtd10@hotmail.com>
2026-01-16 10:13:49 +0100

    Update: jest-setup-after-env.ts

--- FlouraFrontend/FlouraMobileApp/jest-setup-after-env.ts
+++ FlouraFrontend/FlouraMobileApp/jest-setup-after-env.ts
@@ -1,7 +1,11 @@
 import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
 
-import * as safeAreaMock from 'react-native-safe-area-context/jest/mock';
+import * as SafeAreaJestMock from 'react-native-safe-area-context/jest/mock';
+
+// Nogle setups eksporterer mocken som default, andre som namespace.
+// Den her linje gør det robust i CI.
+const safeAreaMock: any = (SafeAreaJestMock as any).default ?? SafeAreaJestMock;
 
 jest.mock('react-native-safe-area-context', () => safeAreaMock);
 jest.mock(
