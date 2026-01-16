92ee7dbb217f5b190a95e629cfe7fb64cffe808a
MissMaaai <nmtd10@hotmail.com>
2026-01-16 13:58:58 +0100

    update: jest-setup-after-env.ts

--- FlouraFrontend/FlouraMobileApp/jest-setup-after-env.ts
+++ FlouraFrontend/FlouraMobileApp/jest-setup-after-env.ts
@@ -1,24 +1,9 @@
 import '@testing-library/jest-native/extend-expect';
-import 'react-native-gesture-handler/jestSetup';
-import * as React from 'react';
-
-console.log('✅ jest-setup-after-env loaded');
-
-const INSETS = { top: 0, bottom: 0, left: 0, right: 0 };
-const SafeAreaInsetsContext = React.createContext(INSETS);
 
 jest.mock('react-native-safe-area-context', () => ({
   __esModule: true,
   SafeAreaProvider: ({ children }: { children: React.ReactNode }) => children,
-  SafeAreaConsumer: ({ children }: any) => children(INSETS),
-  SafeAreaInsetsContext,
-  useSafeAreaInsets: () => INSETS,
+  SafeAreaConsumer: ({ children }: any) =>
+    children({ top: 0, bottom: 0, left: 0, right: 0 }),
+  useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
 }));
-
-jest.mock(
-  'react-native-safe-area-context/Libraries/NativeSafeAreaProvider',
-  () => ({
-    __esModule: true,
-    default: ({ children }: { children: React.ReactNode }) => children,
-  }),
-);
